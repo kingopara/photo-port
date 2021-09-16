1dd68bfd84e8f167fdc44db0d657cefb447a205b
praiseopara <cpopara2010@yahoo.com>
2021-09-16 09:03:08 -0500

    created gallery component, conditionally render the gallery pages and using hooks(useEffect & useState)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+      {
+          name: "commercial",
+          description: "Photos of grocery stores, food trucks, and other commercial projects",
+      },
+      {
+          name: "portriats",
+          description: "Portriats of people in my life"
+      },
+      {
+          name: "food",
+          description: "Delicious delicacies"
+      },
+      {
+          name: "landscape",
+          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+      },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}>
+      </Nav>
       <main>
-        <About></About>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
