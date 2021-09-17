d4b8168fb4753465cf80addab32c11a2ff8cd80c
praiseopara <cpopara2010@yahoo.com>
2021-09-17 09:09:30 -0500

    initialized the state, sync the state and submit form data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -37,6 +38,7 @@ function App() {
       </Nav>
       <main>
         <div>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory} ></Gallery>
           <About></About>
         </div>
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import photo from '../../assets/small/commercial/0.jpg'
+// import photo from '../../assets/small/commercial/0.jpg'
 
 const PhotoList = ({category}) => {
 
